152dfcc2fbaa35a79119c173516f8d1759618f83
Breno <brenoscalzer1@gmail.com>
2020-02-04 11:30:37 -0300

    fix validation

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -4,7 +4,7 @@ const Entry = require('../models/entry')
 module.exports = {
   retakingUser: function(user, survey) {
   	try {
-      const entry = Entry.find({ user: user, survey: survey })
+      const entry = Entry.findOne({ user: user, survey: survey })
       return Boolean(entry)
   	} catch {
   		return false
